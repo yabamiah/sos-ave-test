158de152b61eaf093b348748b83e7416d89d4400
yabamiah <viniciusmendes1019@gmail.com>
2026-04-30 21:24:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav-links");
+
+if (toggle && links) {
+  toggle.addEventListener("click", () => {
+    const isOpen = links.classList.toggle("open");
+    toggle.setAttribute("aria-expanded", String(isOpen));
+  });
+}
